801ec382465bb6bc12497e0061cc0642c0f78fec
Jvilchezru <vilchez.10.2001@gmail.com>
2024-05-10 19:18:38 -0500

    configure middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
+// Start server
 const app = express();
+
+// Use middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
 export default app;

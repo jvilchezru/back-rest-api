3485301215932af382bbacb6f63b5aa44d839e03
Jesus Vilchez <vilchez.10.2001@gmail.com>
2024-05-21 16:30:47 -0500

    change

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,18 +1,18 @@
-import jwt from 'jsonwebtoken';
-import { TOKEN_SECRET } from '../config.js';
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
-    const { token } = req.cookies;
+  const { token } = req.cookies;
 
-    if (!token) return res.status(401).json({ message: 'Authorization denied' });
+  if (!token) return res.status(401).json({ message: "Authorization denied" });
 
-    // Verify token
-    jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token' });
+  // Verify token
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
 
-        // Decode user
-        req.user = user;
+    // Decode user
+    req.user = user;
 
-        next();
-    });
+    next();
+  });
 };
